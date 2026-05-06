8fd11e58ce66ed6986ef1808f7b0a86cc7c9ed8b
raph <raphael@furter.fr>
2026-05-06 14:50:56 +0200

    bouton sépia

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ def charger(container):
 def chargement(event=None):
     return charger(fenetre_principale)
 
+def sepia():
+    import sepia.py
+    
 # Création de la fenêtre principale
 fenetre_principale = tk.Tk()
 fenetre_principale.title("UVSQolor")
@@ -65,6 +68,6 @@ File_new.bind_all("<Control-O>", chargement)
 
 File_Effets = tk.Menu(menubar, tearoff=False)
 menubar.add_cascade(menu=File_Effets, label="Effets")
-
+File_Effets.add_command(label="Filtre sépia", command=sepia)
 fenetre_principale.config(menu=menubar)
 fenetre_principale.mainloop()
